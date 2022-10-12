2da39f0f61fafe676ee5ba241c4538447d397490
조기범 <akfmzh1207@naver.com>
2022-10-12 22:31:30 +0900

    updated Nurbs.py

--- 챕터 10/nurbs.py
+++ 챕터 10/nurbs.py
@@ -1,9 +1,10 @@
 # Nurbs.c
 # OpenGL SuperBible
-# Program by Richard S. Wright Jr.
+# Program by Richard S.Wright Jr.
 
-# include "../../Common/OpenGLSB.h"	#System and OpenGL Stuff
-# include "../../Common/GLTools.h"   #GLTools
+# include "../../Common/OpenGLSB.h"	# System and OpenGL Stuff
+# include "../../Common/GLTools.h"   # GLTools
+
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
@@ -11,48 +12,49 @@ from math import *
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from Common.OpenGLCommon import *
 from Common.TGACommon import *
+
 # NURBS object pointer
-#pNurb = None
-pNurb = gluNewNurbsRenderer()
+pNurb = None
 
 # The number of control points for this curve
-nNumPoints = 4  # 4 X 4
+nNumPoints = 4 # 4 X 4
 
-# Mesh extends four units -6 to +6 along x and y axis
+# Mesh extends four units - 6 to + 6 along x and y axis
 # Lies in Z plane
-#                u  v  (x,y,z)
-ctrlPoints = [[[-6.0, -6.0, 0.0],  # u = 0,	v = 0
-               [-6.0, -2.0, 0.0],  # v = 1
-               [-6.0,  2.0, 0.0],  # v = 2
-               [-6.0,  6.0, 0.0]],  # v = 3
+#u v(x, y, z)
+ctrlPoints = [[[-6.0, -6.0, 0.0],     # u = 0, v = 0
+                [	-6.0, -2.0, 0.0],  #		v = 1
+                [   -6.0,  2.0, 0.0],  #		v = 2
+                [   -6.0,  6.0, 0.0]], #		v = 3
 
-              [[-2.0, -6.0, 0.0],  # u = 1	v = 0
-               [-2.0, -2.0, 8.0],  # v = 1
-               [-2.0,  2.0, 8.0],  # v = 2
-               [-2.0, -2.0, 0.0],  # v = 1
-               [-2.0,  2.0, 0.0],  # v = 2
-               [-2.0,  6.0, 0.0]],  # v = 3
+                [[  -2.0, -6.0, 0.0],  # u = 1	v = 0
+                [   -2.0, -2.0, 8.0],  #		v = 1
+                [   -2.0,  2.0, 8.0],  #		v = 2
+#              [   -2.0, -2.0, 0.0],  #		v = 1
+#              [   -2.0,  2.0, 0.0],  #		v = 2
+                [   -2.0,  6.0, 0.0]], #		v = 3
 
-              [[2.0, -6.0, 0.0],  # u =2		v = 0
-               [2.0, -2.0, 8.0],  # v = 1
-               [2.0,  2.0, 8.0],  # v = 2
-               [2.0, -2.0, 0.0],  # v = 1
-               [2.0,  2.0, 0.0],  # v = 2
-               [2.0,  6.0, 0.0]],  # v = 3
+                [[   2.0, -6.0, 0.0 ], # u =2   v = 0
+                [   2.0, -2.0, 8.0],   #        v = 1
+                [   2.0, 2.0, 8.0],    #        v = 2
+#              [   2.0, -2.0, 0.0],   #        v = 1
+#              [   2.0, 2.0, 0.0],    #        v = 2
+                [   2.0, 6.0, 0.0]],   #        v = 3
 
-              [[6.0, -6.0, 0.0],  # u = 3	v = 0
-               [6.0, -2.0, 0.0],  # v = 1
-               [6.0,  2.0, 0.0],  # v = 2
-               [6.0,  6.0, 0.0]]]  # v = 3
+                [[  6.0, -6.0, 0.0],     # u = 3  v = 0
+                [   6.0, -2.0, 0.0],     #        v = 1
+                [   6.0, 2.0, 0.0],      #        v = 2
+                [   6.0, 6.0, 0.0]]]    #        v = 3
 
 # Knot sequence for the NURB
 Knots = [0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0]
 
-
-
-
 # Called to draw the control points in Red over the NURB
 def DrawPoints():
+
+    global ctrlPoints
+
+    i, j = 0, 0
 
     # Large Red Points
     glPointSize(5.0)
@@ -60,31 +62,33 @@ def DrawPoints():
 
     # Draw all the points in the array
     glBegin(GL_POINTS)
-
     for i in range(0, 4, 1):
         for j in range(0, 4, 1):
             glVertex3fv(ctrlPoints[i][j])
     glEnd()
 
-
 # NURBS callback error handler
 def NurbsErrorHandler(nErrorCode):
 
-    cMessage = []*64
-    # [64];
+    cMessage = [0 for i in range(64)]
 
     # Extract a text message of the error
-    str(cMessage, "NURBS error occured: ")
-    str(cMessage, gluErrorString(nErrorCode))
+    #strcpy(cMessage, "NURBS error occured: ")
+    cMessage = "".join("NURBS error occured: ")
+    #strcat(cMessage, gluErrorString(nErrorCode))
+    errorCode = cMessage + gluErrorString(nErrorCode)
 
     # Display the message to the user
-    glutSetWindowTitle(cMessage)
+    #glutSetWindowTitle(cMessage)
+    glutSetWindowTitle(errorCode)
 
 
 # Called to draw scene
 def RenderScene():
 
-    #Draw in Blue
+    global pNurb, ctrlPoints
+
+    # Draw in Blue
     glColor3ub(0, 0, 220)
 
     # Clear the window with current clearing color
@@ -102,47 +106,47 @@ def RenderScene():
     gluBeginSurface(pNurb)
 
     # Evaluate the surface
-    gluNurbsSurface(pNurb,  # pointer to NURBS renderer
-                    # 8,
-                    Knots,  # No. of knots and knot array u direction
-                    # 8,
-                    Knots,  # No. of knots and knot array v direction
-                    # 4 * 3,				#Distance between control points in u dir.
-                    # 3,					#Distance between control points in v dir.
-                    ctrlPoints,  # Control points
-                    # 4, 4,					#u and v order of surface
-                    GL_MAP2_VERTEX_3)  # Type of surface
-
+    gluNurbsSurface(pNurb, # pointer to NURBS renderer
+    #8, Knots,              # No.of knots and knot array u direction
+    #8, Knots,              # No.of knots and knot array v direction
+    Knots, Knots,
+    #4 * 3,                 # Distance between control points in u dir.
+    #3,                     # Distance between control points in v dir.
+    ctrlPoints, # Control points
+    #4, 4,                  # u and v order of surface
+    GL_MAP2_VERTEX_3)     # Type of surface
+    
     # Done with surface
     gluEndSurface(pNurb)
-
+    
     # Show the control points
     DrawPoints()
-
+    
     # Restore the modelview matrix
     glPopMatrix()
-
+    
     # Dispalay the image
     glutSwapBuffers()
 
-
 # This function does any needed initialization on the rendering
-# context.  Here it sets up and initializes the lighting for
+# context.Here it sets up and initializes the lighting for
 # the scene.
 def SetupRC():
+
+    global pNurb
 
     # Light values and coordinates
     whiteLight = [0.7, 0.7, 0.7, 1.0]
     specular = [0.7, 0.7, 0.7, 1.0]
     shine = [100.0]
-
+    
     # Clear Window to white
-    glClearColor(1.0, 1.0, 1.0, 1.0)
-
+    glClearColor(1.0, 1.0, 1.0, 1.0 )
+    
     # Enable lighting
     glEnable(GL_LIGHTING)
     glEnable(GL_LIGHT0)
-
+    
     # Enable color tracking
     glEnable(GL_COLOR_MATERIAL)
 
@@ -153,49 +157,47 @@ def SetupRC():
 
     # Automatically generate normals for evaluated surfaces
     glEnable(GL_AUTO_NORMAL)
-
+    
     # Setup the Nurbs object
     pNurb = gluNewNurbsRenderer()
-
+    
     # Install error handler to notify user of NURBS errors
     gluNurbsCallback(pNurb, GLU_ERROR, NurbsErrorHandler)
 
     gluNurbsProperty(pNurb, GLU_SAMPLING_TOLERANCE, 25.0)
     # Uncomment the next line and comment the one following to produce a
     # wire frame mesh.
-
-    # //gluNurbsProperty(pNurb, GLU_DISPLAY_MODE, GLU_OUTLINE_POLYGON);
-
+    # gluNurbsProperty(pNurb, GLU_DISPLAY_MODE, GLU_OUTLINE_POLYGON)
     gluNurbsProperty(pNurb, GLU_DISPLAY_MODE, GLU_FILL)
 
-
-def ChangeSize(w,  h):
+def ChangeSize(w, h):
 
     # Prevent a divide by zero
-    if(h == 0):
+    if (h == 0):
         h = 1
-
+    
     # Set Viewport to window dimensions
     glViewport(0, 0, w, h)
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
-
+    
     # Perspective view
-    gluPerspective(45.0, w/h, 1.0, 40.0)
-
+    gluPerspective(45.0, w / h, 1.0, 40.0)
+    
     # Modelview matrix reset
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
     # Viewing transformation, position for better view
-    glTranslatef(0.0, 0.0, -20.0)
+    glTranslatef (0.0, 0.0, -20.0)
 
+if __name__ == '__main__':
 
-glutInit()
-glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
-glutInitWindowSize(800, 600)
-glutCreateWindow("NURBS Surface")
-glutReshapeFunc(ChangeSize)
-glutDisplayFunc(RenderScene)
-SetupRC()
-glutMainLoop()
+    glutInit()
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+    glutInitWindowSize(800, 600)
+    glutCreateWindow(b"NURBS Surface")
+    glutReshapeFunc(ChangeSize)
+    glutDisplayFunc(RenderScene)
+    SetupRC()
+    glutMainLoop()
